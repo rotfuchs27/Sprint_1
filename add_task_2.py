c44fa882a82c275f486590dc67379aa4e2e19110
Julia <YuNikolskaya@skala-r.ru>
2026-02-25 22:31:04 +0300

    clear main branch

--- add_task_2.py
+++
@@ -1,8 +0,0 @@
-def digit_root(num):
-    if num == 0:
-        return 0
-    return 1 + (num - 1) % 9
-
-print('Цифровой корень числа 4851: ', digit_root(4851))  
-print('Цифровой корень числа 97569: ', digit_root(97569))  
-print('Цифровой корень числа 889987: ', digit_root(889987))  
